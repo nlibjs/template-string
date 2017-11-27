e7ce4c3401e9abb2e0e6d0828df6f2df61310575
Kei Ito <kei.itof@gmail.com>
2017-11-27 19:54:38 +0900

    add a test for custom marks

--- test/-transform-template-string/index.js
+++ test/-transform-template-string/index.js
@@ -8,10 +8,12 @@ test('TransformTemplateString', (test) => {
 	[
 		[
 			[
-				'foo[fo',
-				'o]',
-				'bar[ba\n',
-				'r]',
+				[
+					'foo[fo',
+					'o]',
+					'bar[ba\n',
+					'r]',
+				],
 			],
 			{
 				foo: 123,
@@ -19,20 +21,44 @@ test('TransformTemplateString', (test) => {
 			},
 			'foo123bar[ba\nr]',
 		],
+		[
+			[
+				[
+					' <value>fo',
+					'o</value> </val',
+					'ue>[bar]<value> baz<',
+					'/value> </value>',
+				],
+				{
+					open: '<value>',
+					close: '</value>',
+				},
+			],
+			{
+				foo({baz}) {
+					return baz * 3;
+				},
+				bar({baz}) {
+					return baz * 4;
+				},
+				baz: -1,
+			},
+			' -3 </value>[bar]-1 </value>',
+		],
 	]
-	.forEach(([chunks, context, expected]) => {
+	.forEach(([[chunks, options = {}], context, expected]) => {
 		chunks = chunks
 		.map((chunk) => {
 			return Buffer.from(chunk);
 		});
 		const source = Buffer.concat(chunks);
-		test(`${JSON.stringify(source.toString())} (${JSON.stringify(context)}) → ${JSON.stringify(expected)}`, () => {
+		test(`${JSON.stringify(source.toString())} (${JSON.stringify([context, options])}) → ${JSON.stringify(expected)}`, () => {
 			return new Promise((resolve, reject) => {
 				const received = [];
 				let length = 0;
 				const writer = new PassThrough();
 				writer
-				.pipe(new TransformTemplateString(context))
+				.pipe(new TransformTemplateString(context, options))
 				.once('error', reject)
 				.once('end', () => {
 					resolve(Buffer.concat(received, length));
